216bfbb3d1f2eabfeb2e8b786c4d1d3061ccac44
AzuObs <dleaverfr@gmail.com>
2015-11-24 09:56:14 +0700

    enable cors

--- routes/router.js
+++ routes/router.js
@@ -13,8 +13,8 @@
 
 		router.all("*", function(req, res, next) {
 			res.header("Access-Control-Allow-Origin", "*");
-			res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-			res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Token");
+			res.header("Access-Control-Allow-Methods", "*");
+			res.header("Access-Control-Allow-Headers", "*");
 			next();
 		});
 
